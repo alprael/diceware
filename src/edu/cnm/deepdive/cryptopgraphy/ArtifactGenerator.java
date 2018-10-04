d8db58bf3600092a360107e8df604d2f746f92e1
Alex Rael <alprael22@gmail.com>
2018-10-04 13:40:12 -0600

    Initial commit

---
+++ src/edu/cnm/deepdive/cryptopgraphy/ArtifactGenerator.java
@@ -0,0 +1,7 @@
+package edu.cnm.deepdive.cryptopgraphy;
+
+public interface ArtifactGenerator {
+
+  String generate(int length);
+
+}
---
+++ src/edu/cnm/deepdive/cryptopgraphy/WordSource.java
@@ -0,0 +1,9 @@
+package edu.cnm.deepdive.cryptopgraphy;
+
+public interface WordSource {
+
+  String get(int i);
+
+  int size();
+
+}
